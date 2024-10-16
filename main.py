f4707f8f32f4c2bacddf997757dcede64a856cca
Dayan Sone <67210349+sonedayan@users.noreply.github.com>
2024-10-16 03:02:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title= "Movie Listing API",
-    description="An API for managing a list of movies",
+    description="This project is a Movie Listing API built with FastAPI. The API allows authenticated users to list movies, view listed movies, rate them, and add comments. The application is secured using JWT (JSON Web Tokens), ensuring that only the user who listed a movie can edit or delete it.",
 )
 
-app.include_router(router = auth_router,  tags=["Auth"])
+app.include_router(router = auth_router,  tags=["Authentication"])
 app.include_router(router = movie_router, prefix="/api", tags=["Movies"])
 app.include_router(router = rating_router, prefix="/api", tags=["Ratings"])
 app.include_router(router = comment_router, prefix="/api", tags=["Comments"])
